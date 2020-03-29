89a676a6975d8e2516d08cbb5e7b54b071450387
Johannes Liebermann <johannes@kinvolk.io>
2020-03-29 14:35:05 +0200

    Configure logging

--- python/field/field.py
+++ python/field/field.py
@@ -26,20 +26,26 @@ fields = [
 
 class Field(field_pb2_grpc.FieldServicer):
     def GetField(self, request, context):
-        print('Received field request')
+        log = logging.getLogger()
+        log.info('Received field request')
         selected = fields[random.randint(0, len(fields)-1)]
         return field_pb2.FieldReply(field=selected)
 
 
 def serve():
+    log = logging.getLogger()
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     field_pb2_grpc.add_FieldServicer_to_server(Field(), server)
     server.add_insecure_port('[::]:9091')
     server.start()
-    print('Listening for gRPC connections on port 9091')
+    log.info('Listening for gRPC connections on port 9091')
     server.wait_for_termination()
 
 
 if __name__ == '__main__':
-    logging.basicConfig()
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s %(message)s',
+        datefmt='%Y/%m/%d %I:%M:%S',
+    )
     serve()
